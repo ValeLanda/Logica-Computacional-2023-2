82f8825d122341b91ff00d698b04c448824c8306
Brenda Ayala <90218502+Brendaaf2003@users.noreply.github.com>
2023-02-27 10:39:21 -0600

    Carpeta equipo ABFN

---
+++ ABFN/Practica01.hs
@@ -0,0 +1,44 @@
+
+--1. Función esPalindromo: recibe como parámetro una lista y devuelve un booleano, 
+--True en caso de que la lista sea un palíndromo y False en el caso contrario.
+
+
+--2. Función divisores: recibe como parámetro un natural n y devuelve 
+--una lista con todos los divisores de n.
+
+
+--3. Función primos, recibe como parámetro un natural n y devuelve una 
+--lista con todos los números primos entre 1 y n.
+
+
+--4. Función sumaPares, recibe como parámetro una lista de pares ordenados 
+--y devuelve una par con el resultado de sumar todos los pares ordenados de la lista.
+
+
+--5. Función productoCartesiano, recibe como parámetros dos listas, A y B, 
+--y devuelve una lista con el producto cartesiano, AxB.
+
+
+--6. Función aplica, recibe como parámetros una función y una lista, y devuelve 
+--una lista con el resultado de evaluar cada elemento.
+
+
+--7. Función modulo, sin usar la función módulo predefinida en Haskell.
+
+
+--8. Funcipon cadenaPar, recibe como parámetro una cadena y devuelve 
+--True si la longitud de la cadena es par y False en el caso contrario.
+
+
+--9. Función eliminaRepetidos, recibe como parámetro una lista 
+--y devuelve la lista sin repetidos.
+
+
+--10. Función ackermann.
+
+
+
+
+
+
+
---
+++ ABFN/README.md
@@ -0,0 +1,12 @@
+# Practica01
+
+**Integrantes**
+
+* Ayala Flores Brenda - 319051287
+
+* Cervantes Duarte José Fernando - 42
+
+*
+
+* Zavala Velazquez Natalia - 319155189
+
