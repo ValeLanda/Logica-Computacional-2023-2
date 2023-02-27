15f0ced81a89306b0e44a709a0008756e0485fce
Na80t <88749821+Na80t@users.noreply.github.com>
2023-02-27 13:39:45 -0600

    Practica_ABFN

--- ABFN/Practica01.hs
+++ ABFN/Practica01.hs
@@ -9,7 +9,10 @@
 
 --3. Función primos, recibe como parámetro un natural n y devuelve una 
 --lista con todos los números primos entre 1 y n.
-
+primo:: Int -> Bool
+primo x = if (divisores x == [x,1]) then True else False
+primos :: Int -> [Int]
+primos x = [y|y <- divisor x ,primo y == True]
 
 --4. Función sumaPares, recibe como parámetro una lista de pares ordenados 
 --y devuelve una par con el resultado de sumar todos los pares ordenados de la lista.
@@ -17,7 +20,10 @@
 
 --5. Función productoCartesiano, recibe como parámetros dos listas, A y B, 
 --y devuelve una lista con el producto cartesiano, AxB.
-
+productoCartesiano :: [a] -> [b] -> [(a,b)]
+productoCartesiano [] _ = []
+productoCartesiano _ [] = []
+productoCartesiano (x:xs) (y:ys) = (x,y) : productoCartesiano (x:xs) ys ++ productoCartesiano xs (y:ys)
 
 --6. Función aplica, recibe como parámetros una función y una lista, y devuelve 
 --una lista con el resultado de evaluar cada elemento.
@@ -28,6 +34,12 @@
 
 --8. Funcipon cadenaPar, recibe como parámetro una cadena y devuelve 
 --True si la longitud de la cadena es par y False en el caso contrario.
+--cadenaPar['a','a','b','a','a']
+longitudCadena:: String -> Int
+longitudCadena []=0
+longitudCadena (x:xs)=1+longitudCadena xs
+cadenaPar :: String -> Bool
+cadenaPar x= if (mod (longitudCadena x) 2 == 0) then True else False
 
 
 --9. Función eliminaRepetidos, recibe como parámetro una lista 
