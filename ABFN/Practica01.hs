a6798fa44899be61fbee721c374b47a00478ebe0
Na80t <88749821+Na80t@users.noreply.github.com>
2023-02-27 19:18:54 -0600

    Practica_ABFN

--- ABFN/Practica01.hs
+++ ABFN/Practica01.hs
@@ -1,11 +1,22 @@
 
 --1. Función esPalindromo: recibe como parámetro una lista y devuelve un booleano, 
 --True en caso de que la lista sea un palíndromo y False en el caso contrario.
-
+--Asi se tiene que poner en la lista de comandos 
+--palindromo ['a','a','b','a','a']
+reversa :: [a]->[a]
+reversa []=[]
+reversa (x:xs)=reversa xs ++ [x]
+esPalindromo :: (Eq a)=> [a]-> Bool
+esPalindromo []= True
+esPalindromo [x] = True
+esPalindromo xs = if (xs == reversa xs) then True else False
 
 --2. Función divisores: recibe como parámetro un natural n y devuelve 
 --una lista con todos los divisores de n.
-
+divisor 0=[]
+divisor x= (x:(divisor (x-1)))
+divisores :: Int -> [Int]
+divisores x =[y|y <- divisor x ,mod x y == 0 ]
 
 --3. Función primos, recibe como parámetro un natural n y devuelve una 
 --lista con todos los números primos entre 1 y n.
@@ -39,6 +50,7 @@ productoCartesiano (x:xs) (y:ys) = (x,y) : productoCartesiano (x:xs) ys ++ produ
 
 --8. Funcipon cadenaPar, recibe como parámetro una cadena y devuelve 
 --True si la longitud de la cadena es par y False en el caso contrario.
+--Asi se tiene que poner en la lista de comandos
 --cadenaPar['a','a','b','a','a']
 longitudCadena:: String -> Int
 longitudCadena []=0
@@ -49,7 +61,17 @@ cadenaPar x= if (mod (longitudCadena x) 2 == 0) then True else False
 
 --9. Función eliminaRepetidos, recibe como parámetro una lista 
 --y devuelve la lista sin repetidos.
+-- contiene checa si hay un valor en el conjunto 
+-- y si si regresa true si no regresa false
+--Para meter numeros en la terminal se debe ver asi [5,5,5,5,5,5,5,6,7,7,1] 
+--['d','g','g','r','d'] para caracteres 
 
+contiene::(Eq a) => [a]->a ->Bool
+contiene [] n = False
+contiene (x:xs) n = if x==n then True  else contiene xs n
+eliminaRepetidos:: (Eq a) => [a]->[a]
+eliminaRepetidos [] = []
+eliminaRepetidos (x:xs) = if contiene xs x then eliminaRepetidos xs else [x] ++ eliminaRepetidos xs
 
 --10. Función ackermann.
 ackermann :: Int -> Integer -> Integer
