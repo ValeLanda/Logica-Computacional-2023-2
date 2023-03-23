2b3549a8444dae8d74f1626ae73fc13b018f5226
xinwen-zhangliu <xinwen.zhangliu@ciencias.unam.mx>
2023-03-23 13:03:04 -0600

    added data type Elemento

--- lambdas/Practica02.hs
+++ lambdas/Practica02.hs
@@ -8,7 +8,24 @@ module Practica2 where
 {-
 Tipo de datos Elemento deriving Enum
 -}
+data Elemento = Lunes | Martes | Miercoles | Jueves | Viernes | Sabado | Domingo  deriving(Show , Ord , Eq)
 
+instance Enum Elemento where
+toEnum 0 = Lunes
+toEnum 1 = Martes 
+toEnum 2 = Miercoles
+toEnum 3 = Jueves
+toEnum 4 = Viernes
+toEnum 5 = Sabado
+toEnum 6 = Domingo
+
+fromEnum Lunes = 0 
+fromEnum Martes = 1
+fromEnum Miercoles = 2
+fromEnum Jueves = 3
+fromEnum Viernes = 4
+fromEnum Sabado = 5
+fromEnum Domingo = 6
 
 {-
 ArbolBin, arbol binario con informacion en las hojas y en los nodos internos.
@@ -50,17 +67,6 @@ suma a b = if a<b || a>b then Uno
 
 
 
-
-
-
-data Dia = Lunes | Martes | Miercoles deriving(Show , Ord , Eq)
-instance Enum Dia where
-toEnum Lunes = 0
-toEnum Martes = 1
-toEnum Miercoles = 2
-fromEnum 0 = Lunes
-fromEnum 1 = Martes
-fromEnum 2 = Miercoles
 
 
 
