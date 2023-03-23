f6242e0620771e9ae7a0ed21bc7ebc6cb964f9d0
UwU-ntu <malagongasca@ciencias.unam.mx>
2023-03-22 18:25:17 -0600

    Creación de archivo y avances en arbol binario

---
+++ Lorem Ipsum/Practica02.hs
@@ -0,0 +1,17 @@
+module Practica01 where
+
+    data ArbolBin a = Hoja a | Nodo (ArbolBin a) (ArbolBin a) deriving (Show, Eq)
+
+    tamanio :: ArbolBin a -> Int
+    tamanio (Hoja a) = 1
+    tamanio (Nodo a1 a2) = tamanio a1 + tamanio a2
+
+    altura :: ArbolBin a -> Int 
+    altura (Hoja a) = 0
+    altura (Nodo a1 a2) = 1 + max (altura a1) (altura a2)
+
+    ocurrencias :: (Eq a) => ArbolBin a -> a -> Int 
+    ocurrencias (Hoja a) n 
+                    | a == n = 1
+                    | otherwise = 0
+    ocurrencias (Nodo a1 a2) n = ocurrencias a1 n + ocurrencias a2 n
