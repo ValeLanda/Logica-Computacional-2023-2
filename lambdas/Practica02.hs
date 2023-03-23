2e45b617865ed371c4ab57eff88755e59acb07f9
xinwen-zhangliu <xinwen.zhangliu@ciencias.unam.mx>
2023-03-23 15:53:22 -0600

    done

--- lambdas/Practica02.hs
+++ lambdas/Practica02.hs
@@ -13,14 +13,14 @@ data Elemento = Lunes | Martes | Miercoles | Jueves | Viernes | Sabado | Domingo
 
 instance Enum Elemento where
   toEnum 0 = Lunes
-  toEnum 1 = Martes 
+  toEnum 1 = Martes
   toEnum 2 = Miercoles
   toEnum 3 = Jueves
   toEnum 4 = Viernes
   toEnum 5 = Sabado
   toEnum 6 = Domingo
-  
-  fromEnum Lunes = 0 
+
+  fromEnum Lunes = 0
   fromEnum Martes = 1
   fromEnum Miercoles = 2
   fromEnum Jueves = 3
@@ -31,40 +31,56 @@ instance Enum Elemento where
 {-
 ArbolBin, arbol binario con informacion en las hojas y en los nodos internos.
 -}
-data Tree t = Void | Node t (Tree t) (Tree t) deriving (Show)
+data Tree t = Leaf t | Node  (Tree t) t (Tree t) | LN (Tree t) t | RN t (Tree t) deriving (Show)
 
 {- | tamanio
 Recibe un arbol y regresa la cantidad de hojas que contiene.
 -}
 tamanio :: Num p => Tree t -> p
-tamanio Void = 0
-tamanio (Node t (Void) (Void)) = 1
-tamanio (Node t (a) (b))  = tamanio (a) + tamanio (b)
+tamanio (Leaf t) = 1
+tamanio (LN  (Leaf x) t ) = 1
+tamanio (RN t (Leaf x)) = 1
+tamanio (Node  (a) t (b))  = tamanio (a) + tamanio (b)
 
-{- | altura 
+{- | altura
 Recibe un arbol y regresa la altura de este contando la raiz.
 -}
 altura :: (Num p, Ord p) => Tree t -> p
-altura Void = 0
-altura (Node t (a)(b)) = 1 + max (altura a)(altura b)
-                   
+altura (Leaf t) = 0
+altura (LN  (Leaf x) t ) = 1
+altura (RN t (Leaf x)) = 1
+altura (Node  (a) t (b))  = 1 + max (altura (a)) (altura (b))
+
 {- | ocurrecias
 Recibe un elemento y un arbol y recibe el numero de ocurrencias de este en el arbol.
 -}
 ocurrencias :: (Num a, Eq t) => t -> Tree t -> a
-ocurrencias _ Void = 0
-ocurrencias x (Node t (a) (b)) = if x == t
+ocurrencias a (Leaf t) = if a==t then 1 else 0
+ocurrencias x (RN t (b)) = if x == t
+                                 then 1  + ocurrencias x b
+                                 else  ocurrencias x b
+ocurrencias x (LN  (a) t  ) = if x == t
+                                 then 1 + ocurrencias x a
+                                 else  ocurrencias x a
+ocurrencias x (Node (a) t (b)) = if x == t
                                  then 1 + ocurrencias x a + ocurrencias x b
                                  else  ocurrencias x a + ocurrencias x b
 
-{- | aplicaArbol 
+{- | aplicaArbol
 Recibe una funcion y un arbol y aplica dicha funcion a cada nodo del arbol.
 -}
 aplicaArbol :: (t1 -> t2) -> Tree t1 -> Tree t2
-aplicaArbol _ Void = Void
-aplicaArbol f (Node t (a) (b)) = Node (f t)
+aplicaArbol f (Leaf a) = Leaf (f a)
+aplicaArbol f (RN t (b)) = RN (f t)
+                           (aplicaArbol f b)
+aplicaArbol f (LN (a) t ) = LN (aplicaArbol f a)
+                            (f t)
+aplicaArbol f (Node (a) t (b)) = Node
                                  (aplicaArbol f a)
+                                 (f t)
                                  (aplicaArbol f b)
+
+
 
 {-
 Tipo de dato DigBinario, el cual contiene unicaamente al Cero y Uno.
@@ -83,4 +99,3 @@ suma. recibe un NumBinario y regresa la suma binaria.
 suma :: DigBinario -> DigBinario -> DigBinario
 suma a b = if a<b || a>b then Uno
            else Cero
-
