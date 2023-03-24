bb11d34a29507218dfb6bd6ba3bed9739dece3f9
BrunsO27 <bruno.ortiz_a10@ciencias.unam.mx>
2023-03-24 12:47:30 -0600

    Agrupacion de todos los ejercicios en un unico archivo e integracion de comentarios para el ejercicio 3

--- UDBE/Práctica2/Practica02.hs
+++ UDBE/Práctica2/Practica02.hs
@@ -3,13 +3,48 @@
 - Práctica 2
 -}
 
+module Practica02 where
 
---Ejercicio 02
+    --Ejercicio 01
+    {-Crea el tipo de dato Elemento 
+    Define manualmente la relacion para la 
+    clase Enum
+    Establece un orden personalizado
+    El tipo de dato debe de poder mostrarse
+    -}
 
-module Arboles where
 
-    -- La estructura permite Void para representar el arbol vacìo y para
-    -- facilitar su escritura y hacer más legibles los árboles.
+    --Creacion del tipo de dato elementos
+    data Elemento = Agua | Tierra | Aire | Fuego | Metal | Electrico | Diamante | Hielo  deriving(Eq, Show)
+
+    --Orden personalizado de Elementos
+    instance Enum Elemento where  
+        toEnum 1 = Agua 
+        toEnum 2 = Tierra 
+        toEnum 3 = Aire
+        toEnum 4 = Fuego
+        toEnum 5 = Metal
+        toEnum 6 = Electrico
+        toEnum 7 = Diamante
+        toEnum 8 = Hielo
+
+        fromEnum Agua = 1
+        fromEnum Tierra = 2
+        fromEnum Aire = 3
+        fromEnum Fuego = 4
+        fromEnum Metal = 5
+        fromEnum Electrico = 6
+        fromEnum Diamante = 7
+        fromEnum Hielo = 8
+
+    --Ejercicio 02
+    {-ArbolBin. árbol binario con infromacion en 
+      las hojas y ne los nodos internos. 
+      Elige a lo más 4.
+    -}
+
+
+    -- La estructura solo permite crear árboles binarios completos, es decir, donde cada nodo tiene 0 o 2 hijos. 
     data ArbolBin a = Hoja a | Nodo (ArbolBin a) a (ArbolBin a) | NodoConHI (ArbolBin a) a | NodoConHD a (ArbolBin a) deriving (Show, Eq)
 
     -- Función recursiva que cuenta primero el nodo raíz y después cuenta los subárboles con el mismo proceso.
@@ -34,8 +69,30 @@ module Arboles where
     listaHojas (Hoja a) = [a]
     listaHojas (Nodo (hijoI) a (hijoD)) = listaHojas hijoI ++ [a] ++ listaHojas hijoD
     
-    unir :: ArbolBin a -> ArbolBin a -> a -> ArbolBin
+    --Ejercicio 3
+    {-Crea el tipo de dato Digbinario, el cual
+      unicamente debe contener al Cero y al
+      Uno. A partir de DigBinario crea el tipo
+      de dato NumBinario.
+    -}
 
+    --Creación del tipo de dato Digbinario
+    data Digbinario = Cero | Uno
+
+    --Creación del tipo de dato Numbinario
+    data Numbinario = Numbinario [Digbinario]
+
+    --Funcion suma que recibe un Numbinario y regresa la suma binaria
+    sumNumbinario :: Numbinario -> Numbinario -> Numbinario
+    sumNumbinario (Numbinario bs1) (Numbinario bs2) = Numbinario (aBinario bs1 bs2)
+        where
+        aBinario [] bs2 = bs2
+        aBinario bs1 [] = bs1
+        aBinario (b1:bs1) (b2:bs2) = case (b1, b2) of
+            (Cero, Cero) -> Cero : aBinario bs1 bs2
+            (Uno, Cero) -> Uno : aBinario bs1 bs2
+            (Cero, Uno) -> Uno : aBinario bs1 bs2
+            (Uno, Uno) -> Cero : aBinario (aBinario [Uno] bs1) bs2
 
 -----------------------------------------------------------
 ------------------       PRUEBAS       -------------------- 
@@ -52,27 +109,6 @@ module Arboles where
 
 
 
-module Binario where
-
-    {-Ejercicio 3-}
-
-    --Creacion del tipo de dato Digbinario
-    data Digbinario = Cero | Uno
-
-    --Creacion del tipo de dato Numbinario
-    data Numbinario = Numbinario [Digbinario]
-
-    --Creacion de la funcion sumNumbinario
-    sumNumbinario :: Numbinario -> Numbinario -> Numbinario
-    sumNumbinario (Numbinario bs1) (Numbinario bs2) = Numbinario (aBinario bs1 bs2)
-    where
-        aBinario [] bs2 = bs2
-        aBinario bs1 [] = bs1
-        aBinario (b1:bs1) (b2:bs2) = case (b1, b2) of
-            (Cero, Cero) -> Cero : aBinario bs1 bs2
-            (Uno, Cero) -> Uno : aBinario bs1 bs2
-            (Cero, Uno) -> Uno : aBinario bs1 bs2
-            (Uno, Uno) -> Cero : aBinario (aBinario [Uno] bs1) bs2
 
 
 
