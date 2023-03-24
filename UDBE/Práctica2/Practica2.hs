52e8beaf2aa5d872f6d67afcc13b67524a1901a3
BrunsO27 <bruno.ortiz_a10@ciencias.unam.mx>
2023-03-23 23:14:57 -0600

    Integracion del ejercicio 3 en el archivo Practica2.hs

--- UDBE/Práctica2/Practica2.hs
+++ UDBE/Práctica2/Practica2.hs
@@ -50,6 +50,30 @@ module Arboles where
 
 
 
+module Binario where
+
+    {-Ejercicio 3-}
+
+    --Creacion del tipo de dato Digbinario
+    data Digbinario = Cero | Uno
+
+    --Creacion del tipo de dato Numbinario
+    data Numbinario = Numbinario [Digbinario]
+
+    --Creacion de la funcion sumNumbinario
+    sumNumbinario :: Numbinario -> Numbinario -> Numbinario
+    sumNumbinario (Numbinario bs1) (Numbinario bs2) = Numbinario (aBinario bs1 bs2)
+    where
+        aBinario [] bs2 = bs2
+        aBinario bs1 [] = bs1
+        aBinario (b1:bs1) (b2:bs2) = case (b1, b2) of
+            (Cero, Cero) -> Cero : aBinario bs1 bs2
+            (Uno, Cero) -> Uno : aBinario bs1 bs2
+            (Cero, Uno) -> Uno : aBinario bs1 bs2
+            (Uno, Uno) -> Cero : aBinario (aBinario [Uno] bs1) bs2
+
+
+
 
 
 
