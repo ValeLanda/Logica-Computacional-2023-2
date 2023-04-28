07b7d6ed20cfb9e6774ed65a534653c87eb72fef
Asahel <asahelmain@ciencias.unam.mx>
2023-04-28 14:49:23 -0600

    Ejercicio 3

--- AAAA/Practica03/Practica03.hs
+++ AAAA/Practica03/Practica03.hs
@@ -2,4 +2,14 @@
 {-|
    Tipo de dato ExpB
 -}
-data ExpB = V | F | Equal ExpB ExpB | Not ExpB | And ExpB
+data ExpB = V | F | Equal ExpB ExpB | Not ExpB | And ExpB ExpB
+
+{-|
+   Mostrar las expresiones booleanas con operadores usuales
+-}
+instance Show ExpB where
+    show V = "V"
+    show F = "F"
+    show (Equal a b) = show a ++ " ≡ " ++ show b
+    show (Not a) = "¬" ++ show a
+    show (And a b) = show a ++ " ∧ " ++ show b
