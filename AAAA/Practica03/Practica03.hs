6d36a7a71eef2a83a0eee4538a554e8ec86d41e3
Asahel <asahelmain@ciencias.unam.mx>
2023-04-28 14:59:19 -0600

    Ejercicio 3

--- AAAA/Practica03/Practica03.hs
+++ AAAA/Practica03/Practica03.hs
@@ -13,3 +13,23 @@ instance Show ExpB where
     show (Equal a b) = show a ++ " ≡ " ++ show b
     show (Not a) = "¬" ++ show a
     show (And a b) = show a ++ " ∧ " ++ show b
+
+{-|
+   Función que evalúa expresiones booleanas
+-}
+evalB :: ExpB -> Bool
+evalB V = True
+evalB F = False
+evalB (Equal a b) = evalB(a) == evalB(b)
+evalB (Not a) = not(evalB a)
+evalB (And a b) = evalB(a) && evalB(b)
+
+{-|
+   Función que cuenta la cantidad de operadores en una expresión booleana
+-}
+contar :: ExpB -> Int
+contar V = 0
+contar F = 0
+contar (Equal a b) = 1 + contar a + contar b
+contar (Not a) = 1 + contar a
+contar (And a b) = 1 + contar a + contar b
