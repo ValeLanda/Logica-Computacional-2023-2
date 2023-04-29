3548ff03e53e0e00b059af5406e2ebb6f0338f6f
Arturo González Peñaloza <64112938+Artgnz@users.noreply.github.com>
2023-04-28 21:36:37 -0600

    Ejercicios 1 y 2.

--- AAAA/Practica03/Practica03.hs
+++ AAAA/Practica03/Practica03.hs
@@ -1,3 +1,43 @@
+-- 1.
+-- Recibe una cadena y elimina todas las vocales de la misma
+-- devolviendo la cadena resultante.
+eliminarVocales :: String -> String
+eliminarVocales s = [c | c <- s, not $ c  `elem` "aeiouAEIOU"]
+
+-- 2.
+-- Recibe una cadena de texto y un número, y aplica un cifrado César.
+cifrar :: String -> Int -> String
+cifrar cadena numero = map (flip recorrerCaracter numero) cadena
+-- Ejemplos de uso:
+-- cifrar "hola" 12 devuelve "t{xm"
+-- cifrar "llave" 11 devuelve "wwl\129p"
+-- cifrar "Clave" (-3) devuelve "@i^sb"
+-- cifrar "@i^sb" 3 devuelve "Clave"
+-- Ejemplos de composición:
+-- La función se puede usar para cifrar y descifrar cadenas, por ejemplo, para
+-- verificar que la función funcione correctamente, podemos realizar las siguientes
+-- composiciones de funciones.
+-- cifrar (cifrar "Hola" 23) (-23) devuelve "Hola"
+-- cifrar (cifrar "llave" 11) (-11) devuelve "llave"
+-- cifrar (cifrar "Clave" (-3)) (3) devuelve "Clave"
+-- Y de esta forma, podemos observar que la función cifra de manera correcta.
+  
+
+-- Función auxiliar de cifrar que recibe un carácter y un entero y recorre
+-- el carácter la cantidad de posiciones indicadas por el parámetro que recibe.
+-- La función acepta y devuelve carácteres que pertenecen a Unicode
+-- (i.e. el carácter resultante no necesariamente pertenecerá al alfabeto en inglés).
+-- En caso de que el caracter resultante se encuentre fuera de los carácteres posibles,
+-- se aplica aritmética modular para devolver un carácter válido.
+recorrerCaracter :: Char -> Int -> Char
+recorrerCaracter x y
+  | z < 0 = toEnum $ modulo + fromEnum (maxBound :: Char)
+  | otherwise = toEnum modulo
+  where z = fromEnum x + y
+        modulo = (rem z (fromEnum (maxBound :: Char)))
+
+
+
 -- 3.
 {-|
    Tipo de dato ExpB
