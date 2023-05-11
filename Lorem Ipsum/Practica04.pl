591b3f7bad7802466faa210cad6ee1b2cf6eaa61
Jose8888888 <113205910+Jose8888888@users.noreply.github.com>
2023-05-11 15:55:19 -0600

    1a) y 1d)

--- Lorem Ipsum/Practica04.pl
+++ Lorem Ipsum/Practica04.pl
@@ -1,3 +1,10 @@
+% a) Dos personas viven en la misma casa si tienen la misma dirección.
+direccion(andres, copilco).
+direccion(casandra, coyoacan).
+direccion(sancho, copilco).
+
+misma_casa(X,Y) :- direccion(X,Z), direccion(Y,Z).
+ 
 /* Base de conocimiento y relación para ser deportista */
 esdeporte(basquetbol).
 esdeporte(natacion).
@@ -29,6 +36,16 @@ esbritanico(X):- esgales(X); esescoces(X); esirlandes(X); esingles(X).
  * esbritanico(nolan) = true
  * */
 
+% d) Te pueden multar si no has pagado la tenencia de tu automóvil. 
+ha_pagado(andres, tenencia).
+ha_pagado(casandra, luz).
+ha_pagado(sancho, gas).
+ha_pagado(sancho, tenencia).
+
+
+pueden_multar(X) :- not(ha_pagado(X, tenencia)).
+
+
 /* Funcion para saber el mayor elemento de una lista */
 
 esmayor(A,B) :- A >= B.
@@ -47,4 +64,3 @@ fibonacci(N,P) :- N>1,N1 is N-1, N2 is N-2, fibonacci(N1, P1), fibonacci(N2,P2),
  * fibonacci(20,P) = 6765
  * 
  * */
-                                                       
