ffa7441b1e7cc88207e025b2fd3188f2f4196d5a
Arsenicus <arsenicus@padrino>
2023-05-12 19:09:46 -0600

    Inclusion de 1c y 1d en la practica04.pl de AAAA

--- AAAA/Practica04/practica04.pl
+++ AAAA/Practica04/practica04.pl
@@ -9,7 +9,18 @@ vive_en(uziel, alburquerque3).
 practica(uziel, clavados).
 practica(beto, futbol_americano).
 practica(eduardo, basquetbol).
-
+nacionalidad(arsenio, titirilcano).
+nacionalidad(arturo, escoces).
+nacionalidad(asahel, irlandes).
+nacionalidad(uziel, newjersey).
+nacionalidad(alan, gales).
+nacionalidad(julieta, ingles).
+pago_tenencia(arsenio, no).
+pago_tenencia(arturo, no).
+pago_tenencia(asahel, si).
+pago_tenencia(uziel, si).
+pago_tenencia(julieta, si).
+pago_tenencia(alan, no).
 /*a) Dos personas viven en la misma casa si tienen la misma dirección. 
  *  Esto ocurre si dos personas distintas viven en el mismo lugar.
  * */
@@ -20,3 +31,12 @@ viven_misma_casa(Persona1, Persona2) :- vive_en(Persona1, Lugar), vive_en(Person
  * */
 es_deportista(Persona) :- practica(Persona,basquetbol); practica(Persona,futbol_americano).
 
+
+/* c) Eres británico si eres galés, inglés, escocés o irlandés. (Si la nacionalidad de la persona coincide con alguna de las anteriores).*/
+es_britanico(Persona) :- nacionalidad(Persona, ingles).
+es_britanico(Persona) :- nacionalidad(Persona, gales).
+es_britanico(Persona) :- nacionalidad(Persona, escoes).
+es_britanico(Persona) :- nacionalidad(Persona, irlandes).
+
+/* d) Te pueden multar si no has pagado la tenencia de tu automóvil. (Si el pago de la tenencia esta marcagada como no pagada).*/
+multa(Persona):- pago_tenencia(Persona, no).
