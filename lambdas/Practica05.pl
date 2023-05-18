cda10eeecf1028cf9762a57d2294dbd6e25a94c8
xinwen-zhangliu <xinwen.zhangliu@ciencias.unam.mx>
2023-05-18 14:15:10 -0600

    comments

---
+++ lambdas/Practica05.pl
@@ -0,0 +1,24 @@
+/*
+  Funcion combina(L1,L2) que recibe una lista L1 de nuemros y a partir de ella construye una
+  lista L2 
+*/
+
+
+
+/*
+  Funcion convexo(L1) que dev-eulve true si loe lementos de la ista son crecientes al inicio de la lista
+  y decrecientes en el restos de la lista.
+*/
+
+
+
+/*
+  Funcion decimal_binario(D, B) donde D es una numero en base 10 y B es su representacion binaria
+*/
+
+
+
+/*
+  Base de concimiento
+*/ 
+
