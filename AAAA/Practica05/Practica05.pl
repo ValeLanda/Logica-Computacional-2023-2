e216dcf158c783b671f6fe7bcdaf57a36ba2a9e0
Arturo González Peñaloza <64112938+Artgnz@users.noreply.github.com>
2023-05-25 20:14:20 -0600

    Ejercicio 4 práctica 5.

--- AAAA/Practica05/Practica05.pl
+++ AAAA/Practica05/Practica05.pl
@@ -21,3 +21,31 @@ combina_aux([X|XS], N, [Y|YS]) :- combina_aux(XS, N, YS), Y is X+N.
  * y que posteriormente le suma esa cantidad a cada elemento de la lista.
  * */
 combina([X|XS], [Y|YS]) :- sumaLista([X|XS], N), combina_aux(XS, N, YS), Y is X+N.
+
+/*
+ * Ejercicio 4)
+ * */
+% Conocimiento sobre divisibilidad. 
+divide(2,6).
+divide(2,12).
+
+divide(3,6).
+divide(3,12).
+
+divide(6,X) :- divide(2,X), divide(3,X).
+
+% Escribir un predicado que represente
+% este conocimiento y usarlo para responder
+% las siguientes preguntas:
+% ¿Existe algún múltiplo de 2?
+% divide(2, X). Devuelve X = 6, X = 12.
+% Por lo que sí existen múltiplos de 2.
+
+% ¿Cuáles son los divisores de 6?
+% divide(X, 6). Devuelve X = 2, X = 3, X = 6. 
+% Por lo que los divisores de 6 son
+% 2, 3 y 6.
+
+% ¿Conocemos algún múltiplo de 6?
+% divide(6, X). Devuelve X = 6, X = 12.
+% Por lo que los múltiplos de 6 son 6 y 12.
