fe523ffcb87a4c81e0271829dc059d855c1b6b2b
ulisess_ap <ulisessap12@ciencias.unam.mx>
2023-05-25 23:48:29 -0600

    ejercicio 1 y 4

--- UDBE/Practica 05/Practica05.pl
+++ UDBE/Practica 05/Practica05.pl
@@ -1,5 +1,19 @@
 % Practica 05
 
+% Ejercicio01
+
+% Implementa la función combina(L1,L2) que recibe una lista L1 de números
+% y a partir de ella construye L2
+
+combina(L1,L2):-
+    combina(L1,L1,L2).
+
+combina([],_,[]).
+combina([X|Xs],L1,[Y|Ys]):-
+    sum_list(L1,Sum),
+    Y is X + Sum,
+    combina(Xs,L1,Ys).
+
 % Ejercicio 3
 % Funcion decimal_binario(D, B) donde D es un número en base 10 y B su representación binaria
 
@@ -11,6 +25,46 @@ decimal_binario(D, B) :-
     Resultado is D div 2,
     decimal_binario(Resultado, RestoBin),
     append(RestoBin, [Residuo], B).
+
+
+/* Ejercicio04
+Supongamos que tenemos el siguiente conocimiento de sobre divisibilidad:
+2 divide a 6
+2 divide a 12
+3 divide a 6
+3 divide a 12 */
+
+
+% Hechos
+divide(2, 6).
+divide(2, 12).
+divide(3, 6).
+divide(3, 12).
+
+% Un número divisible por 2 y por 3 es divisible por 6
+divide(6, X) :-
+    divide(2, X),
+    divide(3, X).
+
+/*Si un número es divisible por 2 y por 3 entonces es divisible por 6
+Escribir un predicado que represente este conocimiento y usarlo para responder a las siguientes preguntas:
+
+*/
+
+% Existe algún múltiplo de 2?
+existe_multiplo_de_2 :-
+    divide(2, X),
+    write('Sí existe, '), write(X), write(' es múltiplo de 2.').
+
+% Cuáles son los divisores de 6?
+divisores_de_6 :-
+    divide(Y, 6),
+    write(Y), write(' es divisor de 6.'), nl.
+
+% Conocemos algún múltiplo de 6?
+conocemos_multiplo_de_6 :-
+    divide(6, X),
+    write('Sí, '), write(X), write(' es múltiplo de 6.').
 
 
 
